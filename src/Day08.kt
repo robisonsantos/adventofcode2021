a3efb897c5e63a153613e83f6a0c813e991366e2
Robison Santos <robisons@amazon.com>
2021-12-08 15:31:05 -0800

    Simplified parser for day-8 a bit
    Removed the need to store 'int to string' mapping as it was redundant

--- src/Day08.kt
+++ src/Day08.kt
@@ -38,66 +38,57 @@ fun main() {
     //   - if size == 5 => 0
     //   - else => 6
     fun getMapping(codes: List<String>): Map<List<Int>, Int> {
-        val intToString = mutableMapOf<Int, String>()
         val stringToInt = mutableMapOf<List<Int>, Int>()
         val codesBySize = codes.groupByTo(mutableMapOf()) { it.length }
 
         // - size 2 => 1
-        intToString[1] = codesBySize[2]!!.first()
-        stringToInt[codesBySize[2]!!.removeFirst().chars().sorted().toList()] = 1
+        val one = codesBySize[2]!!.removeFirst()
+        stringToInt[one.chars().sorted().toList()] = 1
 
         // - size 4 => 4
-        intToString[4] = codesBySize[4]!!.first()
-        stringToInt[codesBySize[4]!!.removeFirst().chars().sorted().toList()] = 4
+        val four = codesBySize[4]!!.removeFirst()
+        stringToInt[four.chars().sorted().toList()] = 4
 
         // - size 3 => 7
-        intToString[7] = codesBySize[3]!!.first()
         stringToInt[codesBySize[3]!!.removeFirst().chars().sorted().toList()] = 7
 
         // - size 7 => 8
-        intToString[8] = codesBySize[7]!!.first()
         stringToInt[codesBySize[7]!!.removeFirst().chars().sorted().toList()] = 8
 
         // - a string with size 6 - '4'
         //   - if size == 2 => 9
-        val nine = codesBySize[6]!!.first { (it - intToString[4]!!).length == 2 }
-        intToString[9] = nine
+        val nine = codesBySize[6]!!.first { (it - four).length == 2 }
         stringToInt[nine.chars().sorted().toList()] = 9
         codesBySize[6]!!.remove(nine)
 
         // - a string with size 5, where '9' - 5 - '1'
         //   - if size == 0 => 5
-        val five = codesBySize[5]!!.first { (intToString[9]!! - it - intToString[1]!!).isEmpty() }
-        intToString[5] = five
+        val five = codesBySize[5]!!.first { (nine - it - one).isEmpty() }
         stringToInt[five.chars().sorted().toList()] = 5
         codesBySize[5]!!.remove(five)
 
         // - a string with size 5 - '1'
         //   - if size == 3 => 3
         //   - if size == 4 => 2
-        val three = codesBySize[5]!!.first { (it - intToString[1]!!).length == 3 }
-        intToString[3] = three
+        val three = codesBySize[5]!!.first { (it - one).length == 3 }
         stringToInt[three.chars().sorted().toList()] = 3
         codesBySize[5]!!.remove(three)
 
-        val two = codesBySize[5]!!.first { (it - intToString[1]!!).length == 4 }
-        intToString[2] = two
+        val two = codesBySize[5]!!.first { (it - one).length == 4 }
         stringToInt[two.chars().sorted().toList()] = 2
         codesBySize[5]!!.remove(two)
 
         // - '9' - '5' == signal 'c'
-        val signalC = intToString[9]!! - intToString[5]!!
+        val signalC = nine - five
 
         // - a string with size 6 - 'c'
         //   - if size == 5 => 0
         //   - else => 6
         val zero = codesBySize[6]!!.first { (it - signalC).length == 5 }
-        intToString[0] = zero
         stringToInt[zero.chars().sorted().toList()] = 0
         codesBySize[6]!!.remove(zero)
 
         val six = codesBySize[6]!!.first()
-        intToString[6] = six
         stringToInt[six.chars().sorted().toList()] = 6
         codesBySize[6]!!.remove(six)
 
